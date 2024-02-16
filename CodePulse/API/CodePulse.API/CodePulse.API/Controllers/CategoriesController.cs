a17dbf3516d0d0467c24c8c6c6626d2bbb5388ba
Piotr <info@PJO-Automation.co.uk>
2024-02-16 11:49:53 -0800

    Initial Commit

---
+++ CodePulse/API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,52 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace CodePulse.API.Controllers
+{
+    //https://localhost:xxx/api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+
+        //Constructor
+        public CategoriesController(ICategoryRepository categoryRepository) 
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        //
+        [HttpPost]
+        public async Task<IActionResult>CreateCategory(CreateCategoryRequestDTO request)
+        {
+            //Map DTO to Domain Model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await categoryRepository.CreateAsync(category);
+           
+
+            //Domain Model to DTO
+            var response = new CategoryDTO
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+            return Ok(category);
+        }
+    }
+
+    
+    
+}
---
+++ CodePulse/API/CodePulse.API/CodePulse.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CodePulse.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {   
+        //constructor
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //property for classes
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
